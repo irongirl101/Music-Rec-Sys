4554a56e892a2ad8a8fabee9c6f2099830857252
irongirl101 <aditi061806@gmail.com>
2026-03-19 19:50:49 +0530

    spaces added
    nullspaces,column space and rowspace

---
+++ main.py
@@ -0,0 +1,48 @@
+import pandas as pd 
+import sympy 
+import numpy as np 
+import csv
+
+def rref_from_csv(filename):
+    try:
+        df = pd.read_csv(filename,header =None)
+    except: 
+        print("Error Occured")
+    
+    matrix = df.to_numpy(dtype=float) # convert to data fram to numpy array - easier for matrix ops, float to avoid integer division
+    matrix_s = sympy.Matrix(matrix)
+
+    rref,pivots = matrix_s.rref()
+
+    return rref,pivots,matrix_s
+
+def spaces(rref,pivots,matrix_s): 
+    rowspace_basis = []
+    for i in range(rref.rows): 
+        row = rref.row(i)
+        if not row.is_zero_matrix: 
+            rowspace_basis.append(list(row))
+    colspace_basis = [] 
+    for j in pivots: 
+        col = matrix_s.col(j)
+        colspace_basis.append(list(col))
+    nullspace = matrix_s.nullspace()
+    nullspace_basis = [list(i) for i in nullspace]
+
+    return rowspace_basis,colspace_basis,nullspace_basis
+    
+
+filename = "main_data.csv"
+rref,pivots,matrix_s = rref_from_csv(filename)
+#rref == rowspace 
+rowspace  = rref
+if rref is not None: 
+    print(rref)
+    print(pivots)
+rs,cs,ns = spaces(rref,pivots,matrix_s)
+print(rs)
+print(cs)
+print(ns) 
+
+    
+
